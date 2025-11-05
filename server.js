5b21bc48b1056d2f6975fc6405423b40c2f94bea
aB3A4T4 <109865567+aB3A4T4@users.noreply.github.com>
2025-11-05 11:56:19 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
